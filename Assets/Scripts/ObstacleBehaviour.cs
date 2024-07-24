d66125429aa138e933d0f7c4eedb1f1f73f02a14
JayCwk <vipercwk@gmail.com>
2024-07-24 08:16:40 +0800

    Added Basic Tile
    - Added basic tile
    - Added placeholder obstacle

---
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    [Tooltip("How long to wait before restarting the game")]
+    public float waitTime = 2.0f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        // First check if we collided with the player
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            // Destroy the player
+            Destroy(collision.gameObject);
+
+            // Call the function ResetGame after waitTime
+            // has passed
+            Invoke("ResetGame", waitTime);
+        }
+    }
+
+    /// Will restart the currently loaded level
+    private void ResetGame()
+    {
+        // Restarts the current level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
